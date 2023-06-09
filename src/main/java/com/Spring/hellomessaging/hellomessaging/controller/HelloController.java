dc243112a5300c42ade5cf2ddf762f1f59ec2626
OjashviSingh <ojashvisingh365@gmail.com>
2023-06-09 22:39:06 +0530

    Added: GET Request Method and pass name as path variable

--- src/main/java/com/Spring/hellomessaging/hellomessaging/controller/HelloController.java
+++ src/main/java/com/Spring/hellomessaging/hellomessaging/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.Spring.hellomessaging.hellomessaging.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String greetHello(){
-        return "Hello. Welcome to Spring Boot";
+        return "Hello from BridgeLabz";
+    }
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam("name") String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
---
+++ src/main/java/com/Spring/hellomessaging/hellomessaging/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.Spring.hellomessaging.hellomessaging.controller;
+
+public class UserController {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
